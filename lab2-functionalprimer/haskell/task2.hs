4a88c836fd70ef2b248b79d618a9bf2d94fd6105
khanAston <h.khan54@aston.ac.uk>
2024-01-15 22:14:28 +0000

    Minor edit to task2.hs

--- lab2-functionalprimer/haskell/task2.hs
+++ lab2-functionalprimer/haskell/task2.hs
@@ -1,12 +1,4 @@
-func1 = putStrLn("Output of func1")
-
 main = do
     putStrLn "Welcome to the programme. Please enter your name"
     name <- getLine
     putStrLn("Hello " ++ name ++ ", hope you like Haskell.")
-    func1
-
-
-{- --Code for P1-T-d-ii
-
--}
