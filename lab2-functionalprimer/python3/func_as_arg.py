c56fb7474608489cefb9c9c25828aba134bd4cb5
khanAston <h.khan54@aston.ac.uk>
2024-01-15 12:58:21 +0000

    Save edits to func_as_arg.py

--- lab2-functionalprimer/python3/func_as_arg.py
+++ lab2-functionalprimer/python3/func_as_arg.py
@@ -1,10 +1,10 @@
-def sumFunc():
-    return sum([i for i in range(1, 6)]) #Create list of ints from 1 to 5, Haskell equivalent [1..5]
+def listFunc():
+    return [i for i in range(1, 6)] #Create list of ints from 1 to 5, Haskell equivalent [1..5]
 
 def applicatorFunc(inpFunc, s):
     if s=='s':
-        return inpFunc()
+        return sum(inpFunc())
     else:
-        return inpFunc()/5
+        return sum(inpFunc())/5
 
-print(applicatorFunc(sumFunc, 'a'))
+print(applicatorFunc(listFunc, 's'))
