8a1a84bdecb3b84fbd4b8264b677c6f9c4ec981a
Yousaf bazaz <yousafbazaz@gmail.com>
2024-01-30 11:41:20 +0000

    lab 2 task 4

--- lab2-functionalprimer/haskell/task4.hs
+++ lab2-functionalprimer/haskell/task4.hs
@@ -4,8 +4,11 @@ ask prompt =
   putStrLn prompt
   line <- getLine
   if line == ""
-    then ask prompt
-    else putStrLn ("you said: " ++ reverse line)
+    then ask(prompt ++ "!")
+    else if(line == "quit") then putStrLn("quitting...")
+    else do
+      putStrLn ("you said: " ++ reverse line)
+      ask prompt
 
 main :: IO ()
 main =
