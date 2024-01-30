0cdd6ddf29713ed63e48c9cc734d4ea6f1b9c55b
Yousaf bazaz <yousafbazaz@gmail.com>
2024-01-30 12:14:10 +0000

    functions as arguments in python3

--- lab2-functionalprimer/python3/func_as_arg.py
+++ lab2-functionalprimer/python3/func_as_arg.py
@@ -1,10 +1,10 @@
-def listFunc():
-    return [i for i in range(1, 6)] #Create list of ints from 1 to 5, Haskell equivalent [1..5]
+def listFunc(a, b):
+    return [i for i in range(a, b+1)] #Create list of ints from 1 to 5, Haskell equivalent [1..5]
 
-def applicatorFunc(inpFunc, s):
+def applicatorFunc(inpFunc, s, a, b):
     if s=='s':
-        return sum(inpFunc())
+        return sum(inpFunc(a, b))
     else:
-        return sum(inpFunc())/5
+        return sum(inpFunc(a, b))/(b-a+1)
 
-print(applicatorFunc(listFunc, 's'))
+print(applicatorFunc(listFunc, 'a', 1, 10))
