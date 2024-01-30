41d4f1c5ef9b7e31d07da23adb5e989abe721089
Yousaf bazaz <yousafbazaz@gmail.com>
2024-01-30 12:05:36 +0000

    lab 2 guard function as arguments

--- lab2-functionalprimer/haskell/func_as_arg.hs
+++ lab2-functionalprimer/haskell/func_as_arg.hs
@@ -2,7 +2,7 @@
 inpFunc a b = [a..b] 
 
 --Define applicatorFunc
-applicatorFunc inpFunc a b s = if s=='s' then sum (inpFunc a b) else (sum(inpFunc a b))/(b-a+1)
+applicatorFunc inpFunc a b s | s=='s'   = sum(inpFunc a b)  | otherwise=(sum(inpFunc a b))/(b-a+1)
 
 main = do
     let result = applicatorFunc inpFunc 1 10 'a' --Call applicatorFunc with inpFunc and 'a' as args
